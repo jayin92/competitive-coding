ff615524da90ed6371e5c9ad693a1b00f727f530
jayinnn <jayin920805@gmail.com>
2020-02-02 10:51:04 +0800

    選項好像不太好

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 > template and create contest script from [@ToMmyDong](https://github.com/dl8sd11/online-judge)
 
-## 編譯選項
+## 編譯指令
 `g++ $fileName -o $fileNameWithoutExt -O2 -std=c++11 -Werror -Wall -Wextra -Wshadow -Djayinnn`
 
 ## 建立比賽
--- contest/cf1295/pA.cpp
+++ contest/cf1295/pA.cpp
@@ -79,6 +79,20 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 int main () {
     TIME(main);
     IOS();
+    int t;
+    int n;
+    cin >> t;
+    debug(t);
+    while(t--){
+        cin >> n;
+        if(n % 2 == 1) {
+            cout << 7;n -= 3;
+        }
+        while(n > 0){
+            cout << 1;n -= 2;
+        }
+        cout << endl;
+    }
 
     return 0;
 }
