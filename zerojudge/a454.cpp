81ce13170e7f13d21e9ddb18a1bc6aa40165ad54
jayin92 <jayin920805@gmail.com>
2020-03-02 07:35:34 +0800

    a454

--- zerojudge/a454.cpp
+++ zerojudge/a454.cpp
@@ -1,147 +1,75 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+#include <cstring>
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<int, ll> pil;
-typedef pair<ll, int> pli;
-typedef pair<double,double> pdd;
-#define SQ(i) ((i)*(i))
-#define MEM(a, b) memset(a, (b), sizeof(a))
-#define SZ(i) int(i.size())
-#define FOR(i, j, k, in) for (int i=j ; i<(k) ; i+=in)
-#define RFOR(i, j, k, in) for (int i=j ; i>=(k) ; i-=in)
-#define REP(i, j) FOR(i, 0, j, 1)
-#define REP1(i,j) FOR(i, 1, j+1, 1)
-#define RREP(i, j) RFOR(i, j, 0, 1)
-#define ALL(_a) _a.begin(),_a.end()
-#define mp make_pair
-#define pb push_back
-#define eb emplace_back
-#define X first
-#define Y second
-#ifdef jayinnn
-#define TIME(i) Timer i(#i)
-#define debug(...) do{\
-    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
-    _do(__VA_ARGS__);\
-}while(0)
-template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
-template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<", ";_do(_t...);}
-template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
-template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
-{
-    _s<<"{";
-    for(It _it=_ita;_it!=_itb;_it++)
-    {
-        _s<<(_it==_ita?"":",")<<*_it;
-    }
-    _s<<"}";
-    return _s;
-}
-template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
-template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
-#define IOS()
-class Timer {
-private:
-    string scope_name;
-    chrono::high_resolution_clock::time_point start_time;
-public:
-    Timer (string name) : scope_name(name) {
-        start_time = chrono::high_resolution_clock::now();
-    }
-    ~Timer () {
-        auto stop_time = chrono::high_resolution_clock::now();
-        auto length = chrono::duration_cast<chrono::microseconds>(stop_time - start_time).count();
-        double mlength = double(length) * 0.001;
-        debug(scope_name, mlength);
-    }
-};
-#else
-#define TIME(i)
-#define debug(...)
-#define pary(...)
-#define endl '\n'
-#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
-#endif
 
-const ll MOD = 1000000007;
-const ll INF = 0x3f3f3f3f3f3f3f3f;
-const int iNF = 0x3f3f3f3f;
-const ll MAXN = 100005;
+const int MAX_N = 1003;
+const int INF = 475634765;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rudu[MAX_N];
+int dp[MAX_N];
+int pre_max[MAX_N];
+bool visit[MAX_N];
+bool in_que[MAX_N];
+vector<int> chudu[MAX_N];
+queue<int> que;
 
-int n;
-vector<vector<pii>> w;
-vector<int> d;
-void dijkstra(int src){
-    priority_queue<pii, vector<pii>> pq;
-    for(int i=1;i<=n;i++){
-        pq.push(mp(-1, i));
-    }
-
-    d[src] = 0;
-
-    while(!pq.empty()){
-        pii u = pq.top();
-        pq.pop();
-        for(auto i:w[u.second]){
-            int alt = d[u.second] + i.first;
-            if(alt > d[i.second]){
-                d[i.second] = alt;
-                pq.push(mp(alt, i.second));
-            }
-        }
-    }
-    
-}
-
-
-/********** Good Luck :) **********/
-int main () {
-    TIME(main);
-    IOS();
-    int t;
-    cin >> t;
-    while(t--){
-        int w_, k, v;
-        cin >> n;
-        w.resize(n+1);
-        d.resize(n+1, -1);
-        vector<pii> end;
-        for(int i=1;i<=n;i++){
-            cin >> w_ >> k;
-            if(k == 0){
-                end.emplace_back(i, w_);
-            }
-            REP(j, k){
-                cin >> v;
-                w[i].emplace_back(w_, v);
-            }
-        }
-        debug(w);
-        while(true){
-            bool flag = false;
-            for(int i=1;i<=n;i++){
-                if(d[i] == -1){
-                    dijkstra(i);
-                    flag = true;
+void bfs(int root) {
+    int ret=-1;
+    visit[root]=true;
+    que.push(root);
+    while (!que.empty()) {
+        int tmp=que.front();
+        que.pop();
+        dp[tmp]+=pre_max[tmp];
+        in_que[tmp]=false;
+        visit[tmp]=true;
+        int len=chudu[tmp].size();
+        for (int x=0;len>x;x++) {
+            int y=chudu[tmp][x];
+            pre_max[y]=max(pre_max[y],dp[tmp]);
+            rudu[y]--;
+            if (rudu[y]==0) {
+                if (in_que[y]==false) {
+                    que.push(y);
+                    in_que[y]=true;
                 }
             }
-            if(!flag) break;
         }
-        int ans = -1;
-        
-        for(auto i:end){
-            ans = max(ans, d[i.first] + i.second);
-        }
-        cout << ans << endl;
-        debug(d);
     }
-   
-    return 0;
+}
+
+int main () {
+    int T;
+    scanf("%d",&T);
+    while (T--) {
+        memset(rudu,0,sizeof(rudu));//入度 
+        memset(dp,0,sizeof(dp));    //cost
+        memset(pre_max,0,sizeof(pre_max));
+        int n;
+        scanf("%d",&n);
+        for (int x=1;n>=x;x++) {
+            scanf("%d",&dp[x]);
+            chudu[x].clear();
+            visit[x]=false;
+            int i;
+            scanf("%d",&i);
+            for (int y=0;i>y;y++) {
+                int k;
+                scanf("%d",&k);
+                rudu[k]++;
+                chudu[x].push_back(k);
+            }
+        }
+        int ans=-INF;
+        for (int i=1;n>=i;i++) {
+            if (visit[i]==false && rudu[i]==0) bfs(i);
+        }
+        for (int i=1;n>=i;i++) {
+            ans=max(ans,dp[i]);
+        }
+        printf("%d\n",ans);
+    }
 }
