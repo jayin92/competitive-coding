c731f15b86426baef65b4e342b75a0e9102bb418
jayin92 <jayin920805@gmail.com>
2020-09-14 20:00:58 +0800

    明天學科QQ

---
+++ contest/abc176/pE.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<double,double> pdd;
+#define SQ(i) ((i)*(i))
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define SZ(i) int(i.size())
+#define FOR(i, j, k, in) for (int i=j ; i<(k) ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=(k) ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define X first
+#define Y second
+#ifdef jayinnn
+#define TIME(i) Timer i(#i)
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<", ";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+class Timer {
+private:
+    string scope_name;
+    chrono::high_resolution_clock::time_point start_time;
+public:
+    Timer (string name) : scope_name(name) {
+        start_time = chrono::high_resolution_clock::now();
+    }
+    ~Timer () {
+        auto stop_time = chrono::high_resolution_clock::now();
+        auto length = chrono::duration_cast<chrono::microseconds>(stop_time - start_time).count();
+        double mlength = double(length) * 0.001;
+        debug(scope_name, mlength);
+    }
+};
+#else
+#define TIME(i)
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll MOD = 1000000007;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int iNF = 0x3f3f3f3f;
+const ll MAXN = 1005;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+pii d[4] = {mp(1, 0), mp(0, 1), mp(-1, 0), mp(0, -1)};
+
+ll ans = INF;
+
+int n, m;
+pii st;
+pii en;
+bool a[MAXN][MAXN];
+
+void print(bool vis[MAXN][MAXN]){
+    REP(i, n){
+        REP(j, m){
+            cout << vis[i][j];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+int check(pii pos){
+    if(pos.X < 0 || pos.X >= n || pos.Y < 0 || pos.Y >= m) return -1;
+
+    if(a[pos.X][pos.Y] == false) return 0;
+    
+    return 1;
+}
+
+void dfs(pii fa, pii cur, ll mag, bool vis[MAXN][MAXN]){
+    // debug(fa, cur, mag);
+    debug(ans);
+    vis[cur.X][cur.Y] = true;
+    if(ans <= mag) return;
+    if(cur == en){
+        ans = min(ans, mag);
+        return;
+    }
+    bool flag = true;
+    for(int i=0;i<4;i++){
+        pii nxt = mp(cur.X + d[i].X, cur.Y + d[i].Y);
+        debug(cur, nxt, check(nxt));
+        if(check(nxt) == 1 && nxt != fa){
+            flag = false;
+            dfs(cur, nxt, mag, vis);
+        }
+    }
+    if(flag){
+        for(int i=-2;i<=2;i++){
+            for(int j=-2;j<=2;j++){
+                pii nxt = mp(cur.X + i, cur.Y + j);
+                // debug(nxt);
+                print(vis);
+                flag = false;
+                if(check(nxt) == 1 && vis[nxt.X][nxt.Y] == false && nxt != cur){
+                    dfs(cur, nxt, mag+1, vis);
+                }
+            }
+        }
+    }
+}
+
+
+
+/********** Good Luck :) **********/
+int main () {
+    TIME(main);
+    IOS();
+
+    cin >> n >> m;
+    cin >> st.X >> st.Y;
+    cin >> en.X >> en.Y;
+    st.X --;
+    st.Y --;
+    en.X --;
+    en.Y --;
+    bool vis[MAXN][MAXN];
+    char tmp;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> tmp;
+            if(tmp == '.') a[i][j] = true, vis[i][j] = false;
+            else a[i][j] = false, vis[i][j] = true;
+            
+        }
+    }
+    dfs(mp(-1, -1), st, 0, vis);
+    cout << (ans == INF ? -1 : ans) << endl;
+
+
+
+
+    return 0;
+}
---
+++ contest/abc177/pE.cpp
@@ -0,0 +1,157 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<double,double> pdd;
+#define SQ(i) ((i)*(i))
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define SZ(i) int(i.size())
+#define FOR(i, j, k, in) for (int i=j ; i<(k) ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=(k) ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define REP1(i,j) FOR(i, 1, j+1, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define ALL(_a) _a.begin(),_a.end()
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define X first
+#define Y second
+#ifdef jayinnn
+#define TIME(i) Timer i(#i)
+#define debug(...) do{\
+    fprintf(stderr,"%s - %d (%s) = ",__PRETTY_FUNCTION__,__LINE__,#__VA_ARGS__);\
+    _do(__VA_ARGS__);\
+}while(0)
+template<typename T>void _do(T &&_x){cerr<<_x<<endl;}
+template<typename T,typename ...S> void _do(T &&_x,S &&..._t){cerr<<_x<<", ";_do(_t...);}
+template<typename _a,typename _b> ostream& operator << (ostream &_s,const pair<_a,_b> &_p){return _s<<"("<<_p.X<<","<<_p.Y<<")";}
+template<typename It> ostream& _OUTC(ostream &_s,It _ita,It _itb)
+{
+    _s<<"{";
+    for(It _it=_ita;_it!=_itb;_it++)
+    {
+        _s<<(_it==_ita?"":",")<<*_it;
+    }
+    _s<<"}";
+    return _s;
+}
+template<typename _a> ostream &operator << (ostream &_s,vector<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,set<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a> ostream &operator << (ostream &_s,deque<_a> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _a,typename _b> ostream &operator << (ostream &_s,map<_a,_b> &_c){return _OUTC(_s,ALL(_c));}
+template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
+#define IOS()
+class Timer {
+private:
+    string scope_name;
+    chrono::high_resolution_clock::time_point start_time;
+public:
+    Timer (string name) : scope_name(name) {
+        start_time = chrono::high_resolution_clock::now();
+    }
+    ~Timer () {
+        auto stop_time = chrono::high_resolution_clock::now();
+        auto length = chrono::duration_cast<chrono::microseconds>(stop_time - start_time).count();
+        double mlength = double(length) * 0.001;
+        debug(scope_name, mlength);
+    }
+};
+#else
+#define TIME(i)
+#define debug(...)
+#define pary(...)
+#define endl '\n'
+#define IOS() ios_base::sync_with_stdio(0);cin.tie(0)
+#endif
+
+const ll MOD = 1000000007;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const int iNF = 0x3f3f3f3f;
+const ll MAXN = 1000006;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+int gcd(int a, int b){
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+// stores smallest prime factor for every number 
+bool prime[MAXN] = {false}; 
+  
+// Calculating SPF (Smallest Prime Factor) for every 
+// number till MAXN. 
+// Time Complexity : O(nloglogn) 
+void sel(){
+    prime[2] = true;
+    prime[3] = true;
+    prime[5] = true;
+    prime[7] = true;
+    
+    for(int i=8;i<MAXN;i++){
+        bool flag = true;
+        for(int j=2;j*j<i;i++){
+            if(prime[j] && i % j == 0){
+                flag = false;
+                break;
+            }
+        }
+        if(flag) prime[i] = true;
+    }
+}
+
+/********** Good Luck :) **********/
+int main () {
+    TIME(main);
+    IOS();
+    sel();
+    int n;
+    cin >> n;
+    vector<int> a(2);
+    bool vis[MAXN] = {false};
+    bool flag = false;
+    int co = 1;
+    for(int i=0;i<n;i++){
+        int tmp = 0;
+        if(i == 0){
+            cin >> a[i];
+            tmp = a[i];
+        } else if(i == 1){
+            cin >> tmp;
+            co = gcd(tmp, a[0]);
+        } else {
+            cin >> tmp;
+            co = gcd(co, tmp);
+        }
+        if(!flag){
+            for(int j=0;j<MAXN;j++){
+                if(prime[j] && tmp % j == 0){
+                    if(!flag && vis[j] == false){
+                        vis[j] = true;
+                    } else {
+                        flag = true;
+                    }
+                }
+            }
+        }
+       
+    }
+    if(!flag){
+        cout << "pairwise coprime" << endl;
+        return 0;
+    }
+    debug(co);
+    if(co == 1){
+        cout << "setwise coprime" << endl;
+    } else {
+        cout << "not coprime" << endl;
+    }
+    
+
+    return 0;
+}
