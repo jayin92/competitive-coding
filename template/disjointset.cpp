9481d6d64ec20d7a5d570596d561fa1a72121da5
李杰穎 (Jay Lee) <jayin920805@gmail.com>
2022-04-25 03:26:18 +0800

    update

--- template/disjointset.cpp
+++ template/disjointset.cpp
@@ -1,18 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-struct DS{
-    int p[n];
-    void init(int n){
-        for(int i=0;i<n;i++){
-            p[i] = i;
-        }
+const int n = 100;
+
+vector<int> p(n);
+vector<int> sz(n, 1);
+
+void init(){
+    for(int i=0;i<n;i++){
+        p[i] = i;
     }
-    void Find(int x){
-        return x == p[x] ? x : p[x] = Find(x);
-    }
-    void Union(int x, int y){
-        p[Find(x)] = Find(y);
-    }
-};
+}
+
+int find(int x){
+    if(p[x] == x) return x;
+    
+    return p[x] = find(x);
+}
+
+void merge(int x, int y){
+    int fx = find(x);
+    int fy = find(y);
+    if(fx == fy) return;
+    if(sz[fx] < sz[fy]) swap(fx, fy);
+    
+    p[fy] = fx;
+    sz[fx] += sz[fy];
+}
+
+int main(){
+    
+}
